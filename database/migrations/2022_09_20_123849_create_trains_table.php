0271629111eb31ac4335dbb7981f2c1e17d7cdbc
Fabiopanetta02 <panetta.fabio20@gmail.com>
2022-09-20 14:49:03 +0200

    Added create structure table

--- database/migrations/2022_09_20_123849_create_trains_table.php
+++ database/migrations/2022_09_20_123849_create_trains_table.php
@@ -15,6 +15,15 @@ class CreateTrainsTable extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('Azienda', 50);
+            $table->string('Stazione di partenza', 100);
+            $table->string('Stazione di arrivo', 100);
+            $table->float('Orario di partenza', 4, 2);
+            $table->float('Orario di arrivo', 4, 2);
+            $table->string('Codice treno', 20);
+            $table->tinyInteger('Numero carrozze')->unisgned();
+            $table->boolean('In orario')->default(1);
+            $table->boolean('Cancellato')->default(0);
             $table->timestamps();
         });
     }
